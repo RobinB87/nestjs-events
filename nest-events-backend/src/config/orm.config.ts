55c1268fee687c134acfdeb89d7464b197b11614
RobinB87 <robinbette@hotmail.com>
2024-01-04 16:09:58 +0000

    setting up a one to many relationship

--- nest-events-backend/src/config/orm.config.ts
+++ nest-events-backend/src/config/orm.config.ts
@@ -1,6 +1,7 @@
 import { registerAs } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Event } from '../events/event.entity';
+import { Attendee } from 'src/events/attendee.entity';
 
 // export default factory function is required
 // config files can be namespaced by wrapping with registerAs
@@ -13,7 +14,7 @@ export default registerAs(
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    entities: [Event],
+    entities: [Event, Attendee],
     synchronize: true,
   }),
 );
---
+++ nest-events-backend/src/events/attendee.entity.ts
@@ -0,0 +1,21 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Event } from './event.entity';
+
+@Entity()
+export class Attendee {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @ManyToOne(() => Event, (event) => event.attendees, { nullable: false }) // use { nullable: false } when an attendee can not exists without an event
+  // @JoinColumn({ name: 'event_id', referencedColumnName: 'secondary' }) // use when you want to point to a different column
+  event: Event;
+}
