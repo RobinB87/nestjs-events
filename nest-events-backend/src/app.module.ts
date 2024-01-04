243c0a7ca31c0b040029794869790ee2c83ff41f
RobinB87 <robinbette@hotmail.com>
2024-01-04 15:31:42 +0000

    move TypeOrmModuleOptions to separate env files

--- nest-events-backend/src/app.module.ts
+++ nest-events-backend/src/app.module.ts
@@ -1,22 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import ormConfig from './config/orm.config';
+import ormConfigProd from './config/orm.config.prod';
 import { EventsModule } from './events/events.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    EventsModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [Event],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true, // no need to import configmodule in child modules
+      load: [ormConfig],
+      expandVariables: true,
+      // ignoreEnvFile: true, // when you want to ignore env files because stuff comes from e.g. docker
+      // envFilePath: when you want a different name / path for the .env
     }),
+    TypeOrmModule.forRootAsync({
+      // allows registering a config async
+      useFactory:
+        process.env.NODE_ENV === 'production' ? ormConfigProd : ormConfig,
+    }),
+    EventsModule,
   ],
 })
 export class AppModule {}
---
+++ nest-events-backend/src/config/orm.config.ts
@@ -0,0 +1,19 @@
+import { registerAs } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Event } from '../events/event.entity';
+
+// export default factory function is required
+// config files can be namespaced by wrapping with registerAs
+export default registerAs(
+  'orm.config',
+  (): TypeOrmModuleOptions => ({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [Event],
+    synchronize: true,
+  }),
+);
