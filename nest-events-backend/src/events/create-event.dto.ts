47064d0343ca74e20e6c58fa168a99f9fcae6f98
RobinB87 <robinbette@hotmail.com>
2024-01-04 10:21:19 +0000

    validation groups optoins

--- nest-events-backend/src/events/create-event.dto.ts
+++ nest-events-backend/src/events/create-event.dto.ts
@@ -14,3 +14,14 @@ export class CreateEventDto {
   @Length(5, 255)
   address: string;
 }
+
+// can also use validation groups:
+// @Length(5, 255, { groups: ['create'] })
+// @Length(10, 20, { groups: ['update'] })
+
+// controller actions then need:
+// new ValidationPipe({ groups: ['create'] })
+
+// global validation should be off
+
+// can also use @UsePipes on controller action, or on controller itself
