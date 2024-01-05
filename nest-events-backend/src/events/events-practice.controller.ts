25d3b967085e1275b156f8d69b740631a31999b7
RobinB87 <robinbette@hotmail.com>
2024-01-05 09:54:20 +0000

    some examples

--- nest-events-backend/src/events/events-practice.controller.ts
+++ nest-events-backend/src/events/events-practice.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
-import { Like, MoreThan, Repository } from 'typeorm';
-import { Event } from './event.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Like, MoreThan, Repository } from 'typeorm';
+import { Attendee } from './attendee.entity';
+import { Event } from './event.entity';
 
 @Controller('/events-practice')
 export class EventsPracticeController {
   constructor(
     @InjectRepository(Event) private readonly repository: Repository<Event>,
+    @InjectRepository(Attendee)
+    private readonly attendeeRepository: Repository<Attendee>,
   ) {}
 
   @Get()
@@ -35,5 +38,40 @@ export class EventsPracticeController {
       where: { id: 1 },
       relations: ['attendees'],
     });
+  }
+
+  @Get('/practice3')
+  async practice3() {
+    // EXAMPLE 1
+    // const event = await this.repository.findOne({ where: { id: 1 } });
+
+    // EXAMPLE 2
+    const event = new Event();
+    event.id = 1;
+
+    const attendee = new Attendee();
+    attendee.name = 'Johnnyboy the 2nd';
+    attendee.event = event;
+
+    await this.attendeeRepository.save(attendee);
+
+    return event;
+  }
+
+  @Get('/practice4')
+  async practice4() {
+    // EXAMPLE 3 - using cascade
+    // SET { cascade: true } or { cascade: [ "insert", "update" ]} to the @OneToMany entity (Event)
+    // BE careful when using this, because you could for example remove all attendees for an event
+    const event = await this.repository.findOne({
+      where: { id: 1 },
+      relations: ['attendees'],
+    });
+
+    const attendee = new Attendee();
+    attendee.name = 'Using Cascade';
+
+    event.attendees.push(attendee);
+    await this.repository.save(event);
   }
 }
