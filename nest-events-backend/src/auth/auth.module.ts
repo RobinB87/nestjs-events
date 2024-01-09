2f4eee0cabe7eec634e70059360871382aeb7805
RobinB87 <robinbette@hotmail.com>
2024-01-09 13:09:44 +0000

    set up jwt

--- nest-events-backend/src/auth/auth.module.ts
+++ nest-events-backend/src/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
 import { LocalStrategy } from './local.strategy';
 import { User } from './user.entity';
-import { JwtModule } from '@nestjs/jwt';
-import { AuthService } from './auth.service';
 
 @Module({
   imports: [
@@ -16,7 +17,7 @@ import { AuthService } from './auth.service';
       }),
     }),
   ],
-  providers: [LocalStrategy, AuthService],
+  providers: [LocalStrategy, JwtStrategy, AuthService],
   controllers: [AuthController],
 })
 export class AuthModule {}
