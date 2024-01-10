c20da0681782b617e845b7294fed3493fb84e021
RobinB87 <robinbette@hotmail.com>
2024-01-10 14:52:36 +0000

    register new controller

--- nest-events-backend/src/events/events.module.ts
+++ nest-events-backend/src/events/events.module.ts
@@ -3,13 +3,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Event } from '../events/event.entity';
 import { Attendee } from './attendee.entity';
 import { AttendeesService } from './attendees.service';
+import { EventAttendeesController } from './event-attendees.controller';
 import { EventsPracticeController } from './events-practice.controller';
 import { EventsController } from './events.controller';
 import { EventsService } from './events.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Attendee, Event])],
-  controllers: [EventsController, EventsPracticeController],
+  controllers: [
+    EventAttendeesController,
+    EventsController,
+    EventsPracticeController,
+  ],
   providers: [AttendeesService, EventsService],
 })
 export class EventsModule {}
