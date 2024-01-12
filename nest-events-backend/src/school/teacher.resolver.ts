8e651924f2197b93d5391c7dbcc97c61620081c0
RobinB87 <robinbette@hotmail.com>
2024-01-12 13:28:42 +0000

    handle errors via exception filter class

--- nest-events-backend/src/school/teacher.resolver.ts
+++ nest-events-backend/src/school/teacher.resolver.ts
@@ -1,7 +1,7 @@
-import { Query, Resolver } from '@nestjs/graphql';
-import { Teacher } from './teacher.entity';
-import { Repository } from 'typeorm';
+import { Args, Int, Query, Resolver } from '@nestjs/graphql';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Teacher } from './teacher.entity';
 
 @Resolver(() => Teacher)
 export class TeacherResolver {
@@ -13,6 +13,14 @@ export class TeacherResolver {
   @Query(() => [Teacher])
   teachers(): Promise<Teacher[]> {
     return this.teachersRepository.find({
+      relations: ['subjects'],
+    });
+  }
+
+  @Query(() => Teacher)
+  teacher(@Args('id', { type: () => Int }) id: number): Promise<Teacher> {
+    return this.teachersRepository.findOneOrFail({
+      where: { id },
       relations: ['subjects'],
     });
   }
