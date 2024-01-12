52da8b3718915794c78538c3adff47f121b84b77
RobinB87 <robinbette@hotmail.com>
2024-01-12 12:42:18 +0000

    return real data

--- nest-events-backend/src/school/teacher.resolver.ts
+++ nest-events-backend/src/school/teacher.resolver.ts
@@ -1,10 +1,17 @@
 import { Query, Resolver } from '@nestjs/graphql';
 import { Teacher } from './teacher.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Resolver(() => Teacher)
 export class TeacherResolver {
+  constructor(
+    @InjectRepository(Teacher)
+    private readonly teachersRepository: Repository<Teacher>,
+  ) {}
+
   @Query(() => [Teacher])
-  async teachers(): Promise<Teacher[]> {
-    return [] as Teacher[];
+  teachers(): Promise<Teacher[]> {
+    return this.teachersRepository.find();
   }
 }
