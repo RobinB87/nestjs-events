d929e7f84c1ce1150ea9378c9a6ba690f61ba885
RobinB87 <robinbette@hotmail.com>
2024-01-14 10:27:32 +0000

    example where not all fields can be changed

--- nest-events-backend/src/school/input/teacher-edit.input.ts
+++ nest-events-backend/src/school/input/teacher-edit.input.ts
@@ -1,5 +1,9 @@
-import { InputType, PartialType } from '@nestjs/graphql'; // for graphql this one is required! (not @nestjs/mapped-types)
+import { InputType, OmitType, PartialType } from '@nestjs/graphql'; // for graphql this one is required! (not @nestjs/mapped-types)
 import { TeacherAddInput } from './teacher-add.input';
 
 @InputType()
-export class TeacherEditInput extends PartialType(TeacherAddInput) {}
+export class TeacherEditInput extends PartialType(
+  // if you want to make sure only certain properties can't be changed, use OmitType
+  // of course can also always declare your own type
+  OmitType(TeacherAddInput, ['gender']),
+) {}
